9c45f563c9f03f59ea4f538f16839e48955f6cb3
Patrick Liang <pl204511194@yahoo.com>
2022-05-19 12:36:54 -0400

    using refs and state with ts

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const EventComponent: React.FC = () => {
-  return <div>EventComponent</div>;
+  function onChange(event: React.ChangeEvent<HTMLInputElement>) {
+    console.log(event);
+  }
+
+  function onDragStart(event: React.DragEvent<HTMLDivElement>) {
+    console.log(event);
+  }
+  return (
+    <div>
+      <input onChange={onChange} />
+      <div draggable onDragStart={onDragStart}>
+        Drag Me!
+      </div>
+    </div>
+  );
 };
 
 export default EventComponent;
--- src/refs/UserSearch.tsx
+++ src/refs/UserSearch.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const users = [
   { name: "Sarah", age: 20 },
@@ -7,25 +7,33 @@ const users = [
 ];
 
 const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const [name, setName] = useState("");
   const [user, setUser] = useState<{ name: string; age: number } | undefined>();
 
-  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+  useEffect(() => {
+    if (!inputRef.current) {
+      return;
+    }
+    inputRef.current.focus();
+  }, []);
+
+  function onClick() {
     const foundUser = users.find((user) => {
       return user.name === name;
     });
-
     setUser(foundUser);
   }
 
   return (
     <div>
       User Search
-      <form onSubmit={onSubmit}>
-        <input value={name} onChange={(e) => setName(e.target.value)} />
-        <button type="submit">Find User</button>
-      </form>
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={onClick}>Find User</button>
       <div>
         {user && user.name}
         {user && user.age}
